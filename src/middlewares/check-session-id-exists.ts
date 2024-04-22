9a9c1e3a6abac194d38785ae1f312d255dd01057
Eduardo <eduardolustosa26@gmail.com>
2024-04-22 12:16:02 -0300

    first commit

---
+++ src/middlewares/check-session-id-exists.ts
@@ -0,0 +1,21 @@
+import type {
+  FastifyRequest,
+  FastifyReply,
+  HookHandlerDoneFunction,
+} from 'fastify'
+
+export function checkSessionIdExists(
+  request: FastifyRequest,
+  reply: FastifyReply,
+  done: HookHandlerDoneFunction,
+) {
+  const { sessionId } = request.cookies
+
+  if (!sessionId) {
+    return reply.status(401).send({
+      error: 'Unauthorized',
+    })
+  }
+
+  done()
+}
